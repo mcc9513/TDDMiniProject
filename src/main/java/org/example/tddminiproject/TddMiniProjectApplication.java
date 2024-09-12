e8d80f1c6effaa1fd40801fc849d833b25fb6efd
Alex Canady <mcc9513@gmail.com>
2024-09-12 17:01:09 -0400

    Hibernate and SQlite DB

---
+++ src/main/java/org/example/tddminiproject/TddMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.tddminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tddminiproject/TddMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tddminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
